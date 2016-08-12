0cf42d9f4dcb8ea2fd90890b8d46025b9ca943e9
darhoden <darhoden@WS-LAB01_146.univel.univel.br>
2016-08-11 22:24:19 -0300

    Criação dos EJB´s Stateless ProcessVenda e ProcessEntrega #4

---
+++ src/main/java/br/diego/ejb/ProcessEntrega.java
@@ -0,0 +1,14 @@
+package br.diego.ejb;
+
+import javax.ejb.Stateless;
+
+import br.diego.classes.Entrega;
+
+@Stateless
+public class ProcessEntrega {
+
+	public void processarEntrega(Entrega entrega){
+
+	}
+
+}
---
+++ src/main/java/br/diego/ejb/ProcessVenda.java
@@ -0,0 +1,14 @@
+package br.diego.ejb;
+
+import javax.ejb.Stateless;
+
+import br.diego.classes.Venda;
+
+@Stateless
+public class ProcessVenda {
+
+	public void processarVenda(Venda venda){
+
+	}
+
+}
