33872b034b5f7f984be6a4ca30381e5f118dc276
darhoden <darhoden@WS-LAB01_146.univel.univel.br>
2016-08-11 22:00:07 -0300

    Esqueci do #2 infelizmente

--- src/main/java/br/diego/servlet/ServletEntrega.java
+++ src/main/java/br/diego/servlet/ServletEntrega.java
@@ -15,6 +15,7 @@ public class ServletEntrega extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		//esqueci do #2
 		super.doGet(req, resp);
 	}
 
