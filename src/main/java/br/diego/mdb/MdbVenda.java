41f8514a716c027f521785695404ac860c6cac16
RHODEN <RHODEN@univel.br>
2016-08-12 02:27:16 -0300

    Criados os Mdb's para escuta e mostra do nome #6

---
+++ src/main/java/br/diego/mdb/MdbVenda.java
@@ -0,0 +1,46 @@
+package br.diego.mdb;
+
+import java.util.logging.Logger;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+
+import br.diego.classes.Venda;
+
+@MessageDriven(name = "MdbVenda", activationConfig = {
+		@ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "topic/TopicVenda"),
+		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
+		@ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge")
+})
+
+public class MdbVenda implements MessageListener {
+
+	private final static Logger LOGGER = Logger.getLogger(MdbVenda.class.toString());
+
+	@Override
+	public void onMessage(Message receiveMessage) {
+		//trecho passado pelo professor em sala...
+		ObjectMessage mensagem = null;
+		try{
+			if(receiveMessage instanceof ObjectMessage){
+				mensagem = (ObjectMessage) receiveMessage;
+				Venda venda = (Venda) mensagem.getObject();
+			}else{
+				LOGGER.warning("message of wrong type: " +receiveMessage);
+			}
+		}catch (JMSException e){
+			throw new RuntimeException(e);
+		}			
+	}
+
+	//	@Override
+	//	public void onMessage(Message arg0) {
+	//		// TODO Auto-generated method stub
+	//
+	//	}
+
+}
